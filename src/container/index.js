39ba8f751cbe7a4997c8366042ac25e04d2ed23f
Priyal lohar <priyallohar27@gmail.com>
2023-06-17 17:26:51 +0530

    update:- make navbar responsive

---
+++ src/container/index.js
@@ -0,0 +1,7 @@
+export {default as Blog} from './Blog/Blog'
+export {default as Feature} from './Feature/Feature'
+export {default as Footer} from './Footer/Footer'
+export {default as Header} from "./Header/Header"
+export {default as Possibility} from './Possibility/Possibilty'
+export {default as whatGpt3} from './whatGpt3/whatGpt3'
+
---
+++ src/container/whatGpt3/whatGpt3.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const whatGpt3 = () => {
+  return (
+    <div>whatGpt3</div>
+  )
+}
+
+export default whatGpt3
